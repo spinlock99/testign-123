87c0fb2c5f33feadc7580724754fc61e73c2b6b6
Andrew Dixon <andrew@joinhandshake.com>
2017-05-31 18:23:56 -0700

    refactored into a thing of beauty

--- src/apps/show.js
+++ src/apps/show.js
@@ -1,20 +1,3 @@
-import React from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import ReactFilestack from "react-filestack";
-import RaisedButton from "material-ui/RaisedButton";
-import { updateFiles } from "../data/actions";
-import { createSelector } from "reselect";
-
-const getAppId = (state, props) => props.match.params.appId;
-const getApp = state => state.apps;
-
-const getFiles = createSelector(
-  getAppId,
-  getApp,
-  (appId, apps) => apps[appId].files || []
-);
-
 export const AppsShow = connect(
   (state, ownProps) => ({
     apps: state.apps,
@@ -22,25 +5,45 @@ export const AppsShow = connect(
     files: getFiles(state, ownProps)
   }),
   dispatch => bindActionCreators({ updateFiles }, dispatch)
-)(({ apps, appId, files, updateFiles }) => {
-  const handleSuccess = appId => result => updateFiles(appId, result);
-  return(
-    <div>
-      <h3>{apps[appId].name}</h3>
-      {!!files.length && files.map(file => <img key={file.handle} src={file.url} />)}
-      <ReactFilestack
-        apikey={FILESTACK_API_KEY}
-        buttonText="Add Icon"
-        onSuccess={handleSuccess(appId)}
-        render={({ onPick }) => (
-          <div style={{ marginTop: "40vh", textAlign: "center" }}>
-            <RaisedButton
-              label="Upload a File"
-              onClick={onPick}
-            />
-          </div>
-        )}
-      />
-    </div>
-  );
-});
+)(({ apps, appId, files, updateFiles }) =>
+  <div>
+    <AppName name={apps[appId].name} />
+    {!!files.length && files.map(file =>
+      <img key={file.handle} src={file.url} style={imgCenter} />)}
+    <Upload appId={appId} updateFiles={updateFiles} />
+  </div>
+);
+
+const AppName = ({ name }) => <h3 style={{ marginLeft: "5vw" }}>{name}</h3>;
+
+const Upload = ({ appId, updateFiles }) =>
+  <ReactFilestack
+    apikey={FILESTACK_API_KEY}
+    buttonText="Add Icon"
+    onSuccess={result => updateFiles(appId, result)}
+    render={({ onPick }) =>
+      <div style={upload}>
+        <RaisedButton
+          label="Upload a File"
+          onClick={onPick} />
+      </div>}
+  />;
+
+const getAppId = (state, props) => props.match.params.appId;
+const getApp = state => state.apps;
+const getFiles = createSelector(
+  getAppId,
+  getApp,
+  (appId, apps) => apps[appId].files || []
+);
+
+const imgCenter = { width: "90vw", display: "block", margin: "0 auto" };
+const upload = { marginTop: "40vh", textAlign: "center" };
+
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import ReactFilestack from "react-filestack";
+import RaisedButton from "material-ui/RaisedButton";
+import { updateFiles } from "../data/actions";
+import { createSelector } from "reselect";
