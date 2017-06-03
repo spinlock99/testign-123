a6ab508a118791ec887eb3d502c5fd5a8d1cb86b
Andrew Dixon <andrew@joinhandshake.com>
2017-06-03 16:48:01 -0700

    clean up coding style

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,3 +1,40 @@
+export const Apps = connect(
+  (state) => ({
+    name: state.name,
+    apps: Object.keys(state.apps).map(key => state.apps[key])
+  }),
+  (dispatch) => bindActionCreators({ createApp, updateName }, dispatch)
+)(({ apps, createApp, name, updateName }) =>
+  <div style={{ padding: "20px" }}>
+    <TextField
+      value={name}
+      hintText="Enter App Name"
+      fullWidth={true}
+      onKeyUp={event => {
+        if (event.which === 13) {
+          createApp(name);
+          updateName("");
+        } else if (event.which === 8) {
+          updateName(name.slice(0, -1));
+        } else {
+          updateName(name + event.key);
+        }
+      }}
+    />
+    {!!apps.length && apps.map(app => (
+      <div key={app.id}>
+        <Link to={`/apps/${app.id}`}>{app.name}</Link><br />
+      </div>
+    ))}
+    <RaisedButton
+      label="Create App"
+      primary={true}
+      fullWidth={true}
+      onClick={event => createApp(name)}
+    />
+  </div>
+);
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
@@ -6,48 +43,4 @@ import { createApp, updateName } from "../data/actions";
 import Divider from "material-ui/Divider";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
-
-
-export const Apps = connect(
-  function mapStateToProps(state) {
-    const apps = Object.keys(state.apps).map(key => state.apps[key]);
-    return { name: state.name, apps };
-  },
-  function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ createApp, updateName }, dispatch);
-  }
-)(({ apps, createApp, name, updateName }) => {
-  const handleClick = event => createApp(name);
-  function handleKeyUp(event) {
-    if (event.which === 13) {
-      createApp(name);
-      updateName("");
-    } else if (event.which === 8) {
-      updateName(name.slice(0, -1));
-    } else {
-      updateName(name + event.key);
-    }
-  }
-
-  return (
-    <div style={{ padding: "20px" }}>
-      <TextField
-        value={name}
-        hintText="Enter App Name"
-        fullWidth={true}
-        onKeyUp={handleKeyUp}
-      />
-      {!!apps.length && apps.map(app => (
-        <div key={app.id}>
-          <Link to={`/apps/${app.id}`}>{app.name}</Link><br />
-        </div>
-      ))}
-      <RaisedButton
-        label="Create App"
-        primary={true}
-        fullWidth={true}
-        onClick={handleClick}
-      />
-    </div>
-  );
-});
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from "material-ui/Toolbar";
