04aed85a13ccf3c15520449e8c6cd920cc8ad0dd
Andrew Dixon <andrew@joinhandshake.com>
2017-06-12 08:59:13 -0700

    working on login. store token in indexedDB

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,20 @@
-import db from "./data/db";
 import { Apps } from "./apps"
 import { AppsShow } from "./apps/show"
-import { Redirect, Route, withRouter } from "react-router-dom"
-import React from "react"
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import db from "./data/db";
 import { Field, reduxForm } from "redux-form";
 import RaisedButton from "material-ui/RaisedButton";
+import React from "react"
+import { Redirect, Route, withRouter } from "react-router-dom"
 import { TextField } from "redux-form-material-ui";
+
+const centered = { margin: "20px auto", textAlign: "center", width: "100%" }
+const flushRight = { float: "right", marginTop: "20px" };
+
 
 export const Routes = props =>
   <div>
-    <AuthButton />
     <Route path="/login" component={ReduxLogin} />
     <Route path="/about" component={props => <h2>About</h2>} />
     <PrivateRoute exact path="/" component={Apps} />
@@ -30,38 +33,9 @@ const auth = {
     this.authenticated = false
     setTimeout(callBack, 100)
   },
-  authenticate(githubToken, callBack) {
+  authenticate(callBack) {
     this.authenticated = true
-    db.table("githubTokens").add({ token: githubToken }).then(id =>
-      callBack())
-  }
-}
-
-const centered = { margin: "20px auto", textAlign: "center", width: "100%" }
-const handleClick = props => e => auth.signout(o=> props.history.push("/"))
-
-const AuthButton = withRouter(props => auth.authenticated
-  ? <button onClick={handleClick(props)}>Sign Out</button>
-  : <div style={centered}>You are Logged Out</div>
-)
-
-class Login extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { redirectToReferrer: false }
-    this.login = this.login.bind(this)
-  }
-
-  login(values) {
-    console.log("values: ", values)
-    auth.authenticate(values.githubToken, o=> this.setState({ redirectToReferrer: true }))
-  }
-
-  render() {
-    if (this.state.redirectToReferrer) {
-      return <Redirect to={{ pathname: "/" }} />
-    }
-    return <LoginForm onSubmit={this.login} />
+    callBack()
   }
 }
 
@@ -70,7 +44,30 @@ const ReduxLogin = connect(
   dispatch => bindActionCreators({}, dispatch)
 )(props => <Login {...props} />)
 
-const flushRight = { float: "right", marginTop: "20px" };
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { redirectToReferrer: false, loading: true }
+    this.login = this.login.bind(this)
+    db.table("githubTokens").toArray().then( tokens => tokens.length > 0
+      ? auth.authenticate(o=> this.setState({ redirectToReferrer: true, loading: false }))
+      : this.setState({ loading: false }))
+  }
+
+  login(values) {
+    console.log("values: ", values)
+    db.table("githubTokens").add({ githubToken: values.githubToken }).then(
+      id => auth.authenticate(o=> this.setState({ redirectToReferrer: true })))
+  }
+
+  render() {
+    if (this.state.loading) { return <span>loading</span> }
+    if (this.state.redirectToReferrer) {
+      return <Redirect to={{ pathname: "/" }} />
+    }
+    return <LoginForm onSubmit={this.login} />
+  }
+}
 
 const LoginForm = reduxForm({ form: "login" })(props =>
   <form style={{ margin: 20 }} onSubmit={props.handleSubmit}>
