0ff39932880cb440ca572686f55d424ad5cb4ab2
Andrew Dixon <andrew@joinhandshake.com>
2017-06-29 21:41:21 -0700

    make react app default route

--- atomic-apps.js
+++ atomic-apps.js
@@ -4,7 +4,7 @@ var path = require("path")
 
 app.use(express.static(path.join(__dirname + "/bin")))
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + '/bin/index.html'))
 })
 
