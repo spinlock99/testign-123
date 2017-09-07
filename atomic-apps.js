022def47bf7604178bf91c9d44e891e83c67d350
Andrew Dixon <andrew@joinhandshake.com>
2017-09-06 21:31:11 -0700

    fix serving static assets from express
    - serve from bin/ in production
    - compile and load from webpack middleware in development

--- atomic-apps.js
+++ atomic-apps.js
@@ -1,18 +1,11 @@
 var express = require("express")
 var path = require("path")
-var webpack = require("webpack")
-var webpackDevMiddleware = require("webpack-dev-middleware")
-var webpackHotMiddleware = require("webpack-hot-middleware")
-var config = require("./webpack.dev.js")
 
 const bodyParser = require("body-parser")
 const zmq = require("zeromq")
 
 var app = express()
 app.use(bodyParser.json())
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname + "/bin")))
-}
 
 const publisher = zmq.socket("pub")
 publisher.bindSync("tcp://*:5556")
@@ -28,6 +21,35 @@ app.post("/github-webhook", function (req, res) {
   publisher.send(["webhook", JSON.stringify(req.body)])
   res.sendStatus(200)
 })
+
+/*
+ *  Static Assets
+ *
+ *  production -- serve static assets from /bin
+ *  development -- use webpack middleware to compile and serve assets
+ */
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname + "/bin")))
+} else {
+  // require development only packages
+
+  var webpack = require("webpack")
+  var webpackDevMiddleware = require("webpack-dev-middleware")
+  var webpackHotMiddleware = require("webpack-hot-middleware")
+  var config = require("./webpack.dev.js")
+
+  var compiler = webpack(config)
+  app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }))
+  app.use(webpackHotMiddleware(compiler))
+  app.get("*", function (req, res, next) {
+    compiler.outputFileSystem.readFile(path.join(__dirname, "index.html"), function (err, result) {
+      if (err) { return next(err) }
+      res.set('content-type', 'text/html')
+      res.send(result)
+      res.end()
+    })
+  })
+}
 
 app.listen(8080)
 console.log("Atomic Apps listening on port 8080")
