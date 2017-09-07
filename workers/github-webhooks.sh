b71d3a736bc14b44776c0f1304a06f5ab577438b
Andrew Dixon <andrew@joinhandshake.com>
2017-09-06 21:51:25 -0700

    update deploy script called by github webhook to only use npm

--- workers/github-webhooks.sh
+++ workers/github-webhooks.sh
@@ -3,7 +3,7 @@
 cd /home/spinlock/atomic-apps
 git checkout master
 git pull
-yarn install
+npm install
 # zeromq neads node-gyp to run in the post install step. yarn isn't doing this
 # now so we force it with npm.
 npm install zeromq
